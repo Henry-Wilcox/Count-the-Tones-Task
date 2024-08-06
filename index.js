d39915b0e7b1a2a0c46063fc4b1d5457efdd121e
Henry-Wilcox <wilcoxjhenry@gmail.com>
2024-08-06 18:57:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+let counterDisplayElem = document.querySelector('.counter-display')
+let counterPlusElem = document.querySelector('.counter-plus');
+let count = 0
+var x = document.getElementById("myAudio");
+let pauseButton = document.querySelector(".stopping-text")
+let startingText = document.querySelector(".starting-text")
+pauseButton.classList.add('gone');
+// Functionality of Buttons
+
+function playAudio() { 
+    x.play(); 
+    startingText.classList.add('gone');
+    pauseButton.classList.remove('gone');
+      } 
+  
+  function pauseAudio() { 
+    x.pause(); 
+    pauseButton.classList.add('gone');
+    startingText.classList.remove('gone');
+  } 
+
+function updateDisplay(){
+    counterDisplayElem.innerHTML = count;};
+
+
+counterPlusElem.addEventListener("click",()=>{count++; updateDisplay();});
+
+
